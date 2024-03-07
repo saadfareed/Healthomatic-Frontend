d45f9e7dcac105b303729e22138e3044b631a417
saadfareed <2018cs632@student.uet.edu.pk>
2024-03-07 05:14:48 +0500

    fix input field issue

--- src/views/SymptomSubmissionView.vue
+++ src/views/SymptomSubmissionView.vue
@@ -10,7 +10,7 @@
             </label>
             <input
               class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-              id="name" placeholder="Enter your name" v-model="name" />
+              id="name" placeholder="Enter your name" v-model="formData.name" />
             <div data-lastpass-icon-root=""
               style="position: relative !important; height: 0px !important; width: 0px !important; float: left !important;">
             </div>
@@ -19,43 +19,22 @@
             <label class="block mb-2" for="gender">
               Gender
             </label>
-            <button type="button" role="combobox" aria-controls="radix-:R4dtqfnnja:" aria-expanded="false"
-              aria-autocomplete="none" dir="ltr" data-state="closed" data-placeholder=""
-              class="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 text-gray-500 dark:text-gray-400">
-              <span style="pointer-events:none">Select gender</span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="h-4 w-4 opacity-50" aria-hidden="true">
-                <path d="m6 9 6 6 6-6"></path>
-              </svg>
-            </button>
+            <select v-model="formData.gender" id="gender" class="flex bg-white h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
+              <option value="" className="hidden">Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
           </div>
           <div class="mb-4">
             <label class="block mb-2" for="dob">
               Date of Birth
             </label>
-            <button
-              class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 pl-3 text-left font-normal text-gray-500 dark:text-gray-400"
-              type="button" aria-haspopup="dialog" aria-expanded="false" aria-controls="radix-:R4htqfnnja:"
-              data-state="closed">
-              <input
-                class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                placeholder="Enter text here" />
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="ml-auto h-4 w-4 opacity-50">
-                <rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect>
-                <line x1="16" x2="16" y1="2" y2="6"></line>
-                <line x1="8" x2="8" y1="2" y2="6"></line>
-                <line x1="3" x2="21" y1="10" y2="10"></line>
-                <path d="M8 14h.01"></path>
-                <path d="M12 14h.01"></path>
-                <path d="M16 14h.01"></path>
-                <path d="M8 18h.01"></path>
-                <path d="M12 18h.01"></path>
-                <path d="M16 18h.01"></path>
-              </svg>
-            </button>
+            <input
+              class="block bg-white h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              type="date"
+              v-model="formData.birth_date"
+              placeholder="Enter text here" />
           </div>
           <div class="mb-4">
             <label class="block mb-2" for="healthcard">
@@ -63,7 +42,7 @@
             </label>
             <input
               class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-              id="healthcard" placeholder="Enter your healthcard number" />
+              v-model="formData.healthcardNumber" id="healthcard" placeholder="Enter your healthcard number" />
             <div data-lastpass-icon-root=""
               style="position: relative !important; height: 0px !important; width: 0px !important; float: left !important;">
             </div>
@@ -73,8 +52,8 @@
               Appointment Timings
             </label>
             <input
-              class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-              id="appointment" placeholder="Enter preferred appointment timings" />
+              class="h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              type="datetime-local" v-model="formData.appointmentTimings" id="appointment" placeholder="Enter preferred appointment timings" />
           </div>
           <div class="mb-4">
             <label class="block mb-2" for="age">
@@ -82,6 +61,7 @@
             </label>
             <input
               class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+              v-model="formData.age"
               placeholder="Enter your age" />
             <select aria-hidden="true" tabindex="-1"
               style="position: absolute; border: 0px; width: 1px; height: 1px; padding: 0px; margin: -1px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; overflow-wrap: normal;">
@@ -93,18 +73,21 @@
           </div>
           <div class="mb-4">
             <label class="block mb-2" for="symptoms">
-              Describe in your own words, or start typing and select symptoms from the list:
+              Describe symptoms in your own words and add your symptom, or select symptoms from the list:
             </label>
             <div class="flex items-center gap-2 mb-2">
               <div
-                class="inline-flex items-center rounded-full whitespace-nowrap border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80">
+                @click="addDefaultSymptom('fever')"
+                class="inline-flex items-center rounded-full whitespace-nowrap border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-blue-500 hover:text-white hover:cursor-pointer">
                 fever
               </div>
               <div
-                class="inline-flex items-center rounded-full whitespace-nowrap border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80">
+                @click="addDefaultSymptom('chest pain')"
+                class="inline-flex items-center rounded-full whitespace-nowrap border px-2.5 py-0.5 w-fit text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-blue-500 hover:text-white hover:cursor-pointer">
                 chest pain
               </div>
               <button
+                @click="addSymptoms"
                 class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -116,7 +99,7 @@
             </div>
             <input
               class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-              id="symptoms" placeholder="Enter your symptom e.g. chest pain" v-model="symptoms" />
+              id="symptoms" placeholder="Enter your symptom e.g. chest pain" v-model="formData.symptom" />
           </div>
           <button
             class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 w-full bg-blue-500 hover:bg-blue-600 text-white"
@@ -146,8 +129,16 @@ export default {
   setup() {
 
     const router = useRouter();
-    const symptoms = ref('');
-    const name = ref('');
+    const formData = ref({
+      name: '',
+      gender: '',
+      dob: '', // Add default value or use a date picker component
+      healthcardNumber: '',
+      appointmentTimings: '',
+      age: '',
+      symptom:'',
+      symptoms: []
+    });
 
     async function submitForm() {
 
@@ -164,15 +155,15 @@ export default {
       console.log("Adding Patient")
 
       const payload = {
-        name: name.value,
-        gender: "Male",
-        birth_date: "1990-05-15",
-        healthcard_number: "HC123456789",
-        appointment_timings: "2022-03-10T09:00:00",
-        age: 32,
-        priority: priority,
-        symptoms: symptoms.value,
-        diagnoses: diagnoses
+        name: this.formData.name,
+        gender: this.formData.gender,
+        birth_date: this.formData.birth_date,
+        healthcard_number: this.formData.healthcardNumber,
+        appointment_timings: this.formData.appointmentTimings,
+        age: this.formData.age,
+        priority: "saad",
+        symptoms: this.formData.symptoms,
+        diagnoses: "check"
       };
 
       axios.post('http://127.0.0.1:8000/addPatient', payload)
@@ -186,10 +177,22 @@ export default {
         });
     }
 
+    async function addSymptoms () {
+      if ( this.formData.symptom != '') {
+        this.formData.symptoms.push(this.formData.symptom)
+        this.formData.symptom = ''
+      }
+    };
+
+    async function addDefaultSymptom (symptom) {
+      this.formData.symptoms.push(symptom)
+    };
+
     return {
       submitForm,
-      name,
-      symptoms
+      addSymptoms,
+      addDefaultSymptom,
+      formData
     }
   }
 }
