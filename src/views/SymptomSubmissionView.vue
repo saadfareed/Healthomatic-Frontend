821e9f2ae5cc454df4b3b0a48a12a9ee078af58b
Jaswir <jaswirraghoe@gmail.com>
2024-03-07 09:50:56 +0100

    Loading button

--- src/views/SymptomSubmissionView.vue
+++ src/views/SymptomSubmissionView.vue
@@ -88,12 +88,26 @@
               id="symptoms" placeholder="Enter your symptom e.g. chest pain" v-model="formData.symptom" />
             <p v-if="showWarningSymptoms" class="text-red-500">Please enter a symptom.</p>
           </div>
+
+
+
           <button
             class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 w-full bg-blue-500 hover:bg-blue-600 text-white"
             @click="submitForm">
-            Thank you for submitting
+            <p v-if="submitting === false">Thank you for submitting</p>
+            <svg v-if="submitting" aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-white animate-spin"
+              viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="#E5E7EB" />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentColor" />
+            </svg>
+            <p v-if="submitting">Submitting...</p>
           </button>
         </div>
+
         <div class="w-1/2 p-4 flex flex-col gap-4">
           <div class="mb-2">
             <h2 class="text-xl font-semibold mb-4">Enter symptoms in your own words OR start typing</h2>
@@ -102,7 +116,6 @@
               <li>Enter each symptom separately or put them all on one line but separated by commas.</li>
             </ul>
           </div>
-
         </div>
       </div>
     </div>
@@ -145,6 +158,7 @@ export default {
     const showWarningHealthcard = ref(false);
     const showWarningAppointment = ref(false);
     const showWarningDateOfBirth = ref(false);
+    const submitting = ref(false);
 
 
 
@@ -174,6 +188,9 @@ export default {
 
       if (invalidSubmission) return;
 
+      submitting.value = true;
+
+      // Getting priority and diagnoses
       console.log("Getting priority based on symptoms", formData.value.symptom)
       const response = await axios.get(`http://127.0.0.1:8000/getPriorityFromSymptoms/${symptoms.value}`);
       let priority = response.data.urgency;
@@ -186,6 +203,7 @@ export default {
 
       console.log("Adding Patient")
 
+      //Filling payload and sending to backend
       const payload = {
         name: formData.value.name,
         gender: formData.value.gender,
@@ -197,6 +215,7 @@ export default {
         diagnoses: diagnoses
       };
 
+
       axios.post('http://127.0.0.1:8000/addPatient', payload)
         .then(response => {
           console.log(response.data); // Log the response data to the console
@@ -206,6 +225,7 @@ export default {
         .catch(error => {
           console.error('Error:', error); // Log any errors to the console
         });
+
     }
 
     return {
@@ -218,6 +238,7 @@ export default {
       showWarningHealthcard,
       showWarningAppointment,
       showWarningDateOfBirth,
+      submitting
 
     }
   }
